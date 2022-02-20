54296ed6d5eff99e79446a0bc3b73c86a674c25b
Levon Stepanyan <levonmll@gmail.com>
2022-02-20 14:21:02 +0200

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react"
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 const App = () => {
 	return(
-		<div><h1>Hello world</h1></div>
+		<Router>
+		<div>
+		<div><h1>TimeBy</h1></div>
+		<div>
+		<Link to= "/2018">2018</Link>
+		<Link to= "/2019">2019</Link>
+		<Link to= "/2020">2020</Link>
+		<Link to= "/2021">2021</Link>
+		<Link to= "/">2022</Link>
+		</div>
+		</div>
+		</Router>
 	)
 }
 
